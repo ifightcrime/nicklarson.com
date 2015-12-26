be093c3b97882e27c9eec6f5b671c690313a4170
Nick Larson <nick.larson@gmail.com>
2015-12-26 05:18:17 +0000

    gofmt

--- main.go
+++ main.go
@@ -1,29 +1,29 @@
 package main
 
 import (
-  "github.com/codegangsta/negroni"
+	"github.com/codegangsta/negroni"
 
-  "net/http"
-  //"fmt"
-  "html/template"
+	"net/http"
+	//"fmt"
+	"html/template"
 )
 
 func main() {
-  router := http.NewServeMux()
+	router := http.NewServeMux()
 
-  router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-    //fmt.Fprintf(w, "Hello world!")
+	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		//fmt.Fprintf(w, "Hello world!")
 
-    if (r.URL.Path != "/") {
-        http.NotFound(w, r)
-        return
-    }
+		if r.URL.Path != "/" {
+			http.NotFound(w, r)
+			return
+		}
 
-    t, _ := template.ParseFiles("templates/home.tmpl")
-    t.Execute(w, nil)
-  })
+		t, _ := template.ParseFiles("templates/home.tmpl")
+		t.Execute(w, nil)
+	})
 
-  n := negroni.Classic()
-  n.UseHandler(router)
-  n.Run(":8001")
+	n := negroni.Classic()
+	n.UseHandler(router)
+	n.Run(":8001")
 }
